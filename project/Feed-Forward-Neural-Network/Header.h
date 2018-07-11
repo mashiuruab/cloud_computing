0144cb6abdb06b393f5c66e555eacdf3fd363e5b
Mashiur Rahman <mashiur2005@gmail.com>
2018-07-10 20:58:43 -0500

    back up

---
+++ project/Feed-Forward-Neural-Network/Header.h
@@ -0,0 +1,34 @@
+//
+//  Header.h
+//  sfmlMac
+//
+//  Created by Alexis Louis on 23/12/2015.
+//  Copyright © 2015 Alexis Louis. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <deque>
+#include <cmath>
+#include <stdio.h>
+#include <random>
+#include <unistd.h>
+#include <algorithm>
+#include <vector>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+
+
+#include <ResourcePath.hpp>
+#include "Layer.hpp"
+#include "FFN.hpp"
+#include "graphic_f.hpp"
+#include "utility_f.hpp"
+
+#endif /* Header_h */
---
+++ project/Feed-Forward-Neural-Network/main.cpp
@@ -0,0 +1,34 @@
+
+#include "Header.h"
+
+int main()
+{
+
+    //-------XOR EXAMPLE--------//
+    FFN *XORnetwork = new FFN();
+    XORnetwork->initFFN(3, 5, 1);
+    //XORnetwork->about();
+    
+    vector<vector<float>> XOR_APP = {{0,0,1},{0,1,1},{1,0,1},{1,1,1}};
+    vector<vector<float>> XOR_TAR = {{0},{1},{1},{0}};
+    
+    XORnetwork->train_by_error(XOR_APP,XOR_TAR,0.0001);
+    //cout << "Calculation done ... Drawing graph" << endl;
+    drawFront(XORnetwork, 1000); // requires SFML lib
+    
+    
+    
+    //-------DIGIT RECOGNITION EXAMPLE--------//
+    FFN *DIGITnetwork = new FFN();
+    DIGITnetwork->initFFN(55, 20, 10);
+    //DIGITnetwork->about();
+    
+    vector<vector<float>> Xapp = readMatFromFile("samples/Xapp.txt");
+    vector<vector<float>> Ta = readMatFromFile("samples/TA.txt");
+    vector<vector<float>> Xtest = readMatFromFile("samples/Xtest.txt");
+    vector<vector<float>> Tt = readMatFromFile("samples/TT.txt");
+    
+    DIGITnetwork->train_by_iteration(Xapp,Ta,1000);
+    DIGITnetwork->test(Xtest,Tt);
+    
+}
